78c4c9f781d304395fb5a92e7e39a9359ab6ac16
chanwkkk <chanwk18@hotmail.com>
2020-05-30 16:55:58 -0600

    build Event

---
+++ models/event.js
@@ -0,0 +1,8 @@
+class Event{
+    constructor(date, name, user_id, description){
+        this.date = date;
+        this.name = name;
+        this.user_id = user_id;
+        this.description = description
+    }
+}
