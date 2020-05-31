17df06c55f7d512b45db9530afa23ecef45eb776
chanwkkk <chanwk18@hotmail.com>
2020-05-31 01:45:27 -0600

    make event the nested route of user

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const router = express.Router()
 
 const user_routes = require("./user_routes")
 
+
 router.get('/',(req,res) => {
     res.send("this is index.page")
-})
+});
 
-router.use('/users', user_routes)
+router.use('/users', user_routes);
+
 
 module.exports = router
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,9 +1,14 @@
 const express = require('express')
 
+const event_routes = require("./event_routes")
+
 const router = express.Router()
 
 router.get('/',(req,res) => {
     res.send("this is users.page")
 })
 
+router.use('/events', event_routes);
+
 module.exports = router
+
