e4c860e08a6fb1de3f2dd3985e83e130485e3077
chanwkkk <chanwk18@hotmail.com>
2020-05-31 16:49:13 -0600

    add more user routes

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -8,8 +8,16 @@ router.get('/',(req,res) => {
     res.send("this is users.page")
 })
 
+router.get('/new',(req,res) => {
+    res.send("this is users/new.page")
+})
+
 router.get('/:id',(req,res) => {
     res.send("this is users.page")
+})
+
+router.get('/:id/edit',(req,res) => {
+    res.send("this is users/edit.page")
 })
 
 router.use('/:id/events', event_routes);
