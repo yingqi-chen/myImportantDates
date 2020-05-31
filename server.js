728087a985593a63de869d258c8944765da87818
chanwkkk <chanwk18@hotmail.com>
2020-05-31 17:18:10 -0600

    set up view engine

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const listEndpoints = require('express-list-endpoints')
+const path = require('path')
 
 const app = express();
 
@@ -8,6 +9,9 @@ const PORT = 5000;
 const routes = require('./routes')
 
 app.use("/", routes)
+
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'./views') )
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
