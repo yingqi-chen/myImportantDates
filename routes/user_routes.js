c44e8cc9b147e3245e38635eaff47cc813cda6e1
chanwkkk <chanwk18@hotmail.com>
2020-05-31 17:10:18 -0600

    use route chaining to refractor the user_routes code

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -4,21 +4,29 @@ const event_routes = require("./event_routes")
 
 const router = express.Router()
 
-router.get('/',(req,res) => {
+router.route("/")
+  .get((req,res) => {
     res.send("this is users.page")
-})
+  })
+  .post((req,res) => {
+    res.send("this is use post.page")
+  })
 
 router.get('/new',(req,res) => {
     res.send("this is users/new.page")
 })
 
-router.get('/:id',(req,res) => {
-    res.send("this is users.page")
-})
-
 router.get('/:id/edit',(req,res) => {
     res.send("this is users/edit.page")
 })
+
+router.route("/:id")
+  .get((req,res) => {
+    res.send("this is a user.page")
+  })
+  .post((req,res) => {
+    res.send("this is userrr one post.page")
+  })
 
 router.use('/:id/events', event_routes);
 
