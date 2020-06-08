e2148bd1ecf9ebfe5273e9ce61bcb31e610ea663
chanwkkk <chanwk18@hotmail.com>
2020-06-08 16:42:21 -0600

    connect to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const listEndpoints = require('express-list-endpoints')
 const path = require('path')
+const mongoose = require('mongoose')
 
 const app = express();
 
@@ -12,6 +13,13 @@ app.use("/", routes)
 
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'./views') )
+
+mongoose.connect('mongodb://localhost:27017/my_important_dates', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+},()=>{
+   console.log("connect succesfully!")
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
