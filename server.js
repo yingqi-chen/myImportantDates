880c081fe1834b7ad0db77fe7b17d65994035abf
chanwkkk <chanwk18@hotmail.com>
2020-06-08 17:32:25 -0600

    add prettier package and config too

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
-const express = require('express')
-const listEndpoints = require('express-list-endpoints')
-const path = require('path')
-const mongoose = require('mongoose')
-
+const express = require('express');
+const listEndpoints = require('express-list-endpoints');
+const path = require('path');
+const mongoose = require('mongoose');
 const app = express();
-
 const PORT = 5000;
+const routes = require('./routes');
 
-const routes = require('./routes')
-
-app.use("/", routes)
+app.use('/', routes);
 
 app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname,'./views') )
+app.set('views', path.join(__dirname, './views') );
 
 mongoose.connect('mongodb://localhost:27017/my_important_dates', {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-},()=>{
-   console.log("connect succesfully!")
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}, ()=>{
+  console.log('connect succesfully!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`)
-    console.log(listEndpoints(app));
-})
+  console.log(`Listening on PORT ${PORT}`);
+  console.log(listEndpoints(app));
+});
