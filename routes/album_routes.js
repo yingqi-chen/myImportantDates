41e16b8a65e8edbcd0a0efa52dc6e75a7eb289da
chanwkkk <chanwk18@hotmail.com>
2020-06-09 16:58:03 -0600

    config album_routes page

--- routes/album_routes.js
+++ routes/album_routes.js
@@ -2,24 +2,13 @@ const express = require('express');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
-const albumRoutes = require('./album_routes');
-
-router.use('/:id/album', albumRoutes);
 
 router.route('/')
     .get((req, res) => {
-      res.send('this is events.page');
+      res.send('this is albums.page');
     })
     .post((req, res) => {
       res.send('this is events/:id/post.page');
-    });
-
-router.route('/:id')
-    .get((req, res) => {
-      res.send('this is events/:id.page');
-    })
-    .put((req, res) => {
-      res.send('this is events/:id/put.page');
     })
     .delete((req, res) => {
       res.send('this is events/:id/delete.page');
