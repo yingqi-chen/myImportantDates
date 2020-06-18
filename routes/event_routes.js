b44164a78f9fe67cd97da97dadfaf194167c6be6
chanwkkk <chanwk18@hotmail.com>
2020-06-18 12:34:33 -0600

    make sure only the right user can change events

--- routes/event_routes.js
+++ routes/event_routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router({mergeParams: true});
 const albumRoutes = require('./album_routes');
 const {getEvents, createEvents, getEvent, editEvent, deleteEvent, setEvent} = require('../controllers/eventsController');
-const {setUser} = require('../controllers/usersController');
+const {setUser, rightUser} = require('../controllers/usersController');
 
 router.use('/:eventId/album', albumRoutes);
 router.use('/', setUser);
@@ -11,11 +11,11 @@ router.use('/:eventId', setEvent);
 
 router.route('/')
     .get(getEvents)
-    .post(createEvents);
+    .post(rightUser, createEvents);
 
 router.route('/:eventId')
     .get(getEvent)
-    .put(editEvent)
-    .delete(deleteEvent);
+    .put(rightUser, editEvent)
+    .delete(rightUser, deleteEvent);
 
 module.exports = router;
