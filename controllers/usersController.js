ebd6443986e20bcd68a22acb1de2dca0da6f87d4
chanwkkk <chanwk18@hotmail.com>
2020-06-17 21:38:02 -0600

    implement login by using jwt methods

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,12 +1,21 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const login = async (req, res) => {
   User.findOne({email: req.body.email}, (err, user) => {
-    if (user.comparePasswords(req.body.password, user.hashPassword)) {
-      res.send('good pass');
+    if (err) res.json(err.message);
+    if (!user) res.json({'message': 'User not found'});
+    else if (user && user.comparePasswords(req.body.password, user.hashPassword)) {
+      res.json({'token': jwt.sign({
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        eventIDs: user.eventIDs,
+      }, 'secretKey'),
+      });
     } else {
-      res.send('not work');
+      res.send({'message': 'Your password and email doesn\'t match.'});
     }
   });
 };
@@ -55,4 +64,4 @@ const updateUser = (req, res) => {
   });
 };
 
-module.exports = {getUser, createUser, updateUser,login};
+module.exports = {getUser, createUser, updateUser, login};
